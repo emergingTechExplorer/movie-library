2c4df108442d2abd390ef5a10eb74f83727422c2
lakbot <lakshanthad@gmail.com>
2024-06-20 11:16:38 -0700

    Update

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
 // the below will point to localhost:3000 which is root of the application
-router.get("/", (req, res) => {
-  //   res.send("Hello World");
+router.get('/', (req, res) => {
+  //   res.send('Hello World');
   res.render('index');
 });
 
