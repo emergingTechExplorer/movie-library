860282ab5b06c0c2beb196cb9e8d271887a4b662
lakbot <lakshanthad@gmail.com>
2024-06-20 01:18:04 -0700

    Update server.js

--- server.js
+++ server.js
@@ -28,5 +28,5 @@ app.use("/", indexRouter);
 const port = process.env.PORT || 3000
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on ports ${port}`)
 })
