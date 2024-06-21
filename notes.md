760bcd13fe6499e50fd8ca308eb1dc1c94d26937
lakbot <lakshanthad@gmail.com>
2024-06-20 22:07:51 -0700

    Day2

--- notes.md
+++ notes.md
@@ -17,3 +17,11 @@ In mongodb or nosql libraries, a schema is equal to a table in normal sql databa
 form inputs will be shared between both new file and edit file. So we need to create a new partial named _form_fields.ejs inside views/authors. The reason we use "_" because it is a partial file but not a file thats been rendered from the server.
 
 express does not have an easy way to access variables such as author. so we install a library called body-parser
+
+We will use async await for all of our functions to make them cleaner
+
+everything in mongoose and mongodb is done asynchronously, so we need to use await in order to wait for that asynchronous call to be completed
+
+<% in errorMessage.ejs will make sure it will not output to HTML, but only run on server
+
+Author model from mongoose has a ton of different functionality. You can find certain authors, create, update authors
--- routes/authors.js
+++ routes/authors.js
@@ -4,8 +4,32 @@ const router = express.Router();
 const Author = require('../models/author')
 
 // All Authors Route
-router.get('/', (req, res) => {
-    res.render('authors/index');
+router.get('/', async (req, res) => {
+    // search for all the request parameters that we send 
+    // and add them to the search options
+    let searchOptions = {}
+    // here we use req.query instead of req.body because a get request sends
+    // information through the query string.
+    // unlike post request sending through the body
+    if (req.query.name != null && req.query.name !== '') {
+        // regular expression will search part of the text inside of a 
+        // field in the simplest case
+        // 'i' will make sure it is case-insensitive
+        searchOptions.name = new RegExp(req.query.name, 'i')
+    }
+    try {
+        // if find() method is empty, it will get all the authors
+        const authors = await Author.find(searchOptions)
+        // send back the search request to the user so that
+        // it will repopulate these fields for them
+        res.render('authors/index', {
+            authors: authors,
+            searchOptions: req.query
+        });
+
+    } catch {
+        res.redirect('/')
+    }
 })
 
 // New Author Route
@@ -17,9 +41,20 @@ router.get('/new', (req, res) => {
 })
 
 // Create Author Route
-router.post('/', (req, res) => {
-    res.send(req.body.name);
+router.post('/', async (req, res) => {
+    const author = new Author({
+        name: req.body.name
+    })
+    try {
+        const newAuthor = await author.save()
+        // res.redirect(`authors/${newAuthor.id}`)
+        res.redirect('authors')
+    } catch {
+        res.render('authors/new', {
+            author: author,
+            errorMessage: 'Error creating Author'
+        })
+    }
 })
-
 
 module.exports = router;
